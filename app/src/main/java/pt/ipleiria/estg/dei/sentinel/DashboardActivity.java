92cddb704fe1186854b45cdce19d23abcad0a3cd
Tiago Parente <t.parente.32@gmail.com>
2019-11-14 18:15:18 +0000

    "funcional UI"

--- app/src/main/java/pt/ipleiria/estg/dei/sentinel/DashboardActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/sentinel/DashboardActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -28,8 +30,12 @@ public class DashboardActivity extends AppCompatActivity {
     TextView qoa;
     TextView humidade;
     TextView temperatura;
+    TextView utilmaData;
     ProgressBar pb;
     List<String> dateArray;
+    ImageView imageTemp;
+    ImageView imageHum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,50 +49,105 @@ public class DashboardActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         dateArray = new ArrayList<>();
         ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar2);
+        imageTemp = findViewById(R.id.imageViewTemperatura);
+        imageHum = findViewById(R.id.imageViewHumidade);
 
 
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                    //isto esta muito scuffed
-                    int temperaturasSum = 0;
-                    int humidadeSum = 0;
-                    int numTemperatura = 0;
-                    int numHumidade = 0;
-                    //--
-                    for (DataSnapshot datas:dataSnapshot.child("Edificio A").getChildren()) {
-                        dateArray.add(datas.getKey());
-                    }
-                    Collections.sort(dateArray);
-                    for (DataSnapshot latestDate:dataSnapshot.child("Edificio A").child(dateArray.get(dateArray.size()-1)).getChildren()) {
-                            for (DataSnapshot key: latestDate.getChildren()) {
-                                    if(key.getKey().equals("temperatura")){
-                                        temperaturasSum+=Integer.parseInt(key.getValue().toString());
-                                        numTemperatura++;
+                //isto esta muito scuffed
+                int temperaturasSum = 0;
+                int humidadeSum = 0;
+                int numTemperatura = 0;
+                int numHumidade = 0;
+                float mediaHum = 0;
+                float mediaTemp = 0;
+                int hora = 0;
+                String data ;
+                //--
+                for (DataSnapshot datas:dataSnapshot.child("Edificio A").getChildren()) {
+                    dateArray.add(datas.getKey());
+                }
+                Collections.sort(dateArray);
+                data = dateArray.get(dateArray.size()-1);
+                for (DataSnapshot latestDate:dataSnapshot.child("Edificio A").child(dateArray.get(dateArray.size()-1)).getChildren()) {
+                        for (DataSnapshot key: latestDate.getChildren()) {
+                                if(key.getKey().equals("temperatura")){
+                                    temperaturasSum+=Integer.parseInt(key.getValue().toString());
+                                    numTemperatura++;
+                                }
+                                if(key.getKey().equals("humidade")){
+                                    humidadeSum+=Integer.parseInt(key.getValue().toString());
+                                    numHumidade++;
+                                }
+                                if(key.getKey().equals("hora")){
+                                    if(Integer.parseInt(key.getValue().toString())>hora){
+                                        hora = Integer.parseInt(key.getValue().toString());
                                     }
-                                    if(key.getKey().equals("humidade")){
-                                        humidadeSum+=Integer.parseInt(key.getValue().toString());
-                                        numHumidade++;
-                                    }
-                            }
-                    }
-                    temperatura.setText(""+(temperaturasSum/numTemperatura));
-                    humidade.setText(""+(humidadeSum/numHumidade));
+                                }
+                        }
+                }
+                mediaTemp = (temperaturasSum/numTemperatura);
+                mediaHum = (humidadeSum/numHumidade);
 
-                /*String humi = dataSnapshot.child("Edificio A").child("humidade").getValue().toString();
-                String temp = dataSnapshot.child("Edificio A").child("temperatura").getValue().toString();
 
-                String qualityOfAir = dataSnapshot.child("Edificio A").child("qualidade").getValue().toString();
+                temperatura.setText(String.valueOf(mediaTemp)+"ºC");
+                humidade.setText(String.valueOf(mediaHum));
+                stats.setText("Temperatura: " + mediaTemp+ "ºC\r\n" + "Humidade: " +mediaHum);
+                //data ta a dar erro idk
+                //utilmaData.setText(data + " - " + hora +"H");
 
-                stats.setText("Temperatura: " + temp+ "\r\n" + "Humidade: " +humi);
-                humidade.setText(humi);
-                temperatura.setText(temp);
-                qoa.setText(qualityOfAir);
-                ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", pb.getProgress(), Integer.parseInt(qualityOfAir));
-                animation.setDuration(1000);
-                animation.setInterpolator(new DecelerateInterpolator());
-                animation.start();*/
+
+
+                if(mediaTemp>=19 && mediaTemp <=35){
+                    //verde
+                    imageTemp.setBackgroundColor(Color.parseColor("#90EE90"));
+                }else if(mediaTemp<19 || mediaTemp>=35 ){
+                    //vermelho
+                    imageTemp.setBackgroundColor(Color.parseColor("#8E1600"));
+
+                }
+
+                if(mediaHum>=50 && mediaHum <=75){
+                    //verde
+                    imageHum.setBackgroundColor(Color.parseColor("#90EE90"));
+                }else if(mediaHum<50 || mediaHum>=75 ){
+                    //vermelho
+                    imageHum.setBackgroundColor(Color.parseColor("#8E1600"));
+
+                }
+
+
+
+                if((mediaTemp>=19 && mediaTemp <=35) && (mediaHum>=50 && mediaHum <=75 )){
+                    //roda a verde
+                    qoa.setText("Good");
+                    //vai para 20%
+                    ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", pb.getProgress(), 20);
+                    animation.setDuration(1000);
+                    animation.setInterpolator(new DecelerateInterpolator());
+                    animation.start();
+                }else if(((mediaTemp<19 || mediaTemp>=35) && (mediaHum>=50 && mediaHum <=75)) ||(
+                        (mediaTemp>=19 && mediaTemp <=35) &&(mediaHum<50 || mediaHum>=75))){
+                    //roda a amarelo
+                    qoa.setText("Average");
+                    //vai para 50%
+                    ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", pb.getProgress(), 50);
+                    animation.setDuration(1000);
+                    animation.setInterpolator(new DecelerateInterpolator());
+                    animation.start();
+                }else{
+                    //roda a vermelho
+                    qoa.setText("Bad");
+                    //vai para 99%
+                    ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", pb.getProgress(), 99);
+                    animation.setDuration(1000);
+                    animation.setInterpolator(new DecelerateInterpolator());
+                    animation.start();
+                }
+
             }
 
             @Override
